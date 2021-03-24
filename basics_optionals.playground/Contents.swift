ab7c8443705af211a8f82f65fbab979c3cba0daa
SwiftGoose5 <swiftgoose19@gmail.com>
2021-03-24 16:13:31 -0400

    Updated optionals playground

--- basics_optionals.playground/Contents.swift
+++ basics_optionals.playground/Contents.swift
@@ -1,3 +1,28 @@
 import Cocoa
 
-var str = "Hello, playground"
+var num: Int
+var numOpt: Int?
+
+num = 5
+numOpt = 6
+
+print(num)
+print(numOpt)
+
+numOpt = nil
+print(numOpt)
+
+print(numOpt ?? 3)
+
+numOpt = 2
+//print(numOpt!)
+
+numOpt = 3
+if var secondNum = numOpt {
+    print(secondNum)
+    secondNum = secondNum * 2
+    print(secondNum)
+} else {
+    print("Something went wrong")
+}
+
